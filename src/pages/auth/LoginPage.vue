84e8c45ea6b403bbfeaa61f4100ed623b2e2c31d
unknown <ThatMythicPerson>
2024-12-12 23:48:11 +0330

    d

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -3,22 +3,63 @@
     <q-page class = "window-width window-height row justify-center item-center">
       <div class="columns">
         <div class = "row">
-        <h5 class = "text-h5 text-width q-my-md">Welcome to my homemade shop</h5>
+        <h5 class = "bg-blue text-h5 text-width q-my-md">Welcome to my homemade shop</h5>
         </div>
         <div class = "row">
           <q-card square bordered class = "q-pa-lg w-400">
             <q-card-section>
               <q-form class = "q-gutter-md">
+                <q-input
+                 v-model = "loginParameter.email"
+                 standout="bg-teal text-white"
+                 square
+                 filled
+                 clearable
+                 type="email"
+                 label="Email"
+                 />
+                 <q-input
+                 v-model="loginParameter.username"
+                 standout="bg-teal text-white"
+                 square
+                 filled
+                 clearable
+                 type="text"
+                 label="Username"
+                 />
+                 <q-input
+                 v-model="loginParameter.password"
+                 standout="bg-teal text-white"
+                 square
+                 filled
+                 clearable
+                 type="password"
+                 label="Password"
+                 />
 
               </q-form>
+            </q-card-section>
+            <q-card-section>
+              <q-btn
+              @click="login()"
+              unelevated
+              color="light-blue-8"
+              size="lg"
+              class="full-width"
+              label="Login"
+              />
+              <q-btn
+              @click = "register()"
+              unelevated
+              color="purple"
+              size="lg"
+              class="full-width q-mt-sm"
+              label="Register"
+              />
             </q-card-section>
           </q-card>
         </div>
       </div>
-        <q-card class = "w-400 mt-200 q-pa-lg">
-           <q-input v-model="userParameter" type="text" label="Username"  />
-           <q-input v-model="userParameter" type="text" label="Password" />
-        </q-card>
     </q-page>
   </q-page-container>
 </template>
@@ -28,16 +69,19 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
-const router = useRouter();
 
-const userParameter = ref({
+
+const loginParameter = ref({
+    email : <string>'',
     username : <string>'',
-    password : <string>''
+    password : <string>'',
 });
 
 const login = () => {
-    console.log(1234);
+    console.log(loginParameter.value.email,loginParameter.value.username,loginParameter.value.password);
 }
+
+const router = useRouter();
 
 const register = () => {
     router.replace({name:'register'})
--- src/pages/auth/RegisterPage.vue
+++ src/pages/auth/RegisterPage.vue
@@ -1,9 +1,104 @@
 <template>
-    <q-page>
-
+  <q-page-container>
+    <q-page class = "window-width window-height row justify-center item-center">
+      <div class="columns">
+        <div class = "row">
+        <h5 class = "bg-blue text-h5 text-width q-my-md">Welcome to my homemade shop</h5>
+        </div>
+        <div class = "row">
+          <q-card square bordered class = "q-pa-lg w-400">
+            <q-card-section>
+              <q-form class = "q-gutter-md">
+                <q-input
+                v-model="registerParameter.name"
+                 square
+                 filled
+                 clearable
+                 type="text"
+                 label="First & Last name"
+                 />
+                 <q-input
+                 v-model="registerParameter.username"
+                 standout="bg-teal text-white"
+                 square
+                 filled
+                 clearable
+                 type="text"
+                 label="Username"
+                 />
+                 <q-input
+                 v-model="registerParameter.password"
+                 standout="bg-teal text-white"
+                 square
+                 filled
+                 clearable
+                 type="password"
+                 label="Password"
+                 />
+                 <q-file
+                 v-model="registerParameter.avatar"
+                 filled
+                 bottom-slots
+                 label="Avatar"
+                 counter
+                  >
+                  <template v-slot:append>
+                   <q-avatar>
+                     <img src="https://preview.redd.it/should-i-ask-in-r-place-if-we-make-a-spamton-head-just-for-v0-hgvs7s650jdb1.jpg?width=640&crop=smart&auto=webp&s=29e09063cfbe2523ee1f73c8693e305441e8bf5d">
+                   </q-avatar>
+                  </template>
+                 </q-file>
+              </q-form>
+            </q-card-section>
+            <q-card-section>
+              <q-btn
+              @click="register()"
+              unelevated
+              color="light-blue-8"
+              size="lg"
+              class="full-width"
+              label="Register"
+              />
+              <q-btn
+              @click = "login()"
+              unelevated
+              color="purple"
+              size="lg"
+              class="full-width q-mt-sm"
+              label="Login"
+              />
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
     </q-page>
+  </q-page-container>
 </template>
 
 <script lang = "ts" setup>
+
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+
+
+const registerParameter = ref({
+    name : <string>'',
+    username : <string>'',
+    password : <string>'',
+    avatar:undefined,
+});
+
+const register = () => {
+  router.replace({name:'register'})
+  console.log(registerParameter.value.name,registerParameter.value.username,registerParameter.value.password,registerParameter.value.avatar);
+}
+
+const router = useRouter();
+const login = () => {
+  router.replace({name:'login'})
+
+}
+
 
 </script>
